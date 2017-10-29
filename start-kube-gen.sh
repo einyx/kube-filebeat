46d5f9fc2a82496409946ecb39c8daeae1579bec
Alessio Garofalo <alessio.garofalo@personio.de>
2017-10-29 07:47:46 +0000

    Suppress output logging

--- start-kube-gen.sh
+++ start-kube-gen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sleep 1
-exec kube-gen -watch -type pods -wait 2s:10s -post-cmd '/app/kill-filebeat.sh' /app/filebeat.yml.tmpl /app/filebeat.yml
+sleep 10
+exec kube-gen -watch -type pods -wait 2s:10s -post-cmd '/app/kill-filebeat.sh' /app/filebeat.yml.tmpl /app/filebeat.yml > /dev/null
